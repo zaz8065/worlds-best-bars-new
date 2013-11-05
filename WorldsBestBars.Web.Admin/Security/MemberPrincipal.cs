1fde7440928c9f3917fe1e81cd98980ec07559ca
Brendan McKenzie <brendan@mckenzie.io>
2013-11-05 11:37:07 -0500

    housekeeping.

--- WorldsBestBars.Web.Admin/Security/MemberPrincipal.cs
+++ WorldsBestBars.Web.Admin/Security/MemberPrincipal.cs
@@ -5,10 +5,24 @@ using WorldsBestBars.Services;
 
 namespace WorldsBestBars.Web.Admin.Security
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public class MemberPrincipal : IPrincipal
     {
+        /// <summary>
+        /// Gets the identity of the current principal.
+        /// </summary>
+        /// <returns>The <see cref="T:WorldsBestBars.Web.Admin.Security.MemberIdentity" /> object associated with the current principal.</returns>
         public IIdentity Identity { get; private set; }
 
+        /// <summary>
+        /// Determines whether the current principal belongs to the specified role.
+        /// </summary>
+        /// <param name="role">The name of the role for which to check membership.</param>
+        /// <returns>
+        /// true if the current principal is a member of the specified role; otherwise, false.
+        /// </returns>
         public bool IsInRole(string role)
         {
             if (Identity is MemberIdentity)
@@ -19,6 +33,11 @@ namespace WorldsBestBars.Web.Admin.Security
             return false;
         }
 
+        /// <summary>
+        /// Gets the specified identifier.
+        /// </summary>
+        /// <param name="id">The identifier.</param>
+        /// <returns></returns>
         public static MemberPrincipal Get(Guid id)
         {
             var entity = new UserService().GetSummary(id);
@@ -37,19 +56,46 @@ namespace WorldsBestBars.Web.Admin.Security
         }
     }
 
+    /// <summary>
+    /// 
+    /// </summary>
     public class MemberIdentity : IIdentity
     {
+        /// <summary>
+        /// Gets the type of authentication used.
+        /// </summary>
+        /// <returns>The type of authentication used to identify the user.</returns>
         public string AuthenticationType
         {
             get { return "internal"; }
         }
 
+        /// <summary>
+        /// Gets or sets the identifier of the current user.
+        /// </summary>
+        /// <value>
+        /// The database identifier of the current user.
+        /// </value>
         public Guid Id { get; set; }
 
+        /// <summary>
+        /// Gets a value that indicates whether the user has been authenticated.
+        /// </summary>
+        /// <returns>true if the user was authenticated; otherwise, false.</returns>
         public bool IsAuthenticated { get; set; }
 
+        /// <summary>
+        /// Gets the name of the current user.
+        /// </summary>
+        /// <returns>The name of the user on whose behalf the code is running.</returns>
         public string Name { get; set; }
 
+        /// <summary>
+        /// Gets or sets the roles.
+        /// </summary>
+        /// <value>
+        /// The roles.
+        /// </value>
         public string[] Roles { get; set; }
     }
 }
